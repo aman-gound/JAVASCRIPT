92f5389aab6a0119fe3adfd16dd9d732101999fe
Aman Gound <amangound726482@gmail.com>
2026-01-06 08:41:16 +0530

    Add files via upload

---
+++ Circular Progress Bar/script.js
@@ -0,0 +1,15 @@
+let circularProgress = document.querySelector(".circular-progress"),
+  progressvalue = document.querySelector(".progress-value");
+let progressStartValue = 0,
+  progressEndValue = 90;
+speed = 50;
+
+let progress = setInterval(() => {
+  progressStartValue++;
+  progressvalue.textContent = `${progressStartValue}%`;
+  circularProgress.style.background = `conic-gradient(#7d2ae8 ${progressStartValue * 3.6}deg, #ededed 0deg)`;
+  if (progressStartValue === progressEndValue) {
+    clearInterval(progress);
+  }
+
+}, speed);
